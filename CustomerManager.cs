7a8b5ef3dbb1779999dd6691e3eea5c083052c2a
ilkay gül <ilkaygl@outlook.com>
2021-01-18 01:38:43 +0300

    Add project files.

---
+++ CustomerManager.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    public class CustomerManager
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine("Yeni Müşteri Eklendi!");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Müşteri ID : " + customer.Id);
+            Console.WriteLine("Müşteri Adı : " + customer.FirstName);
+            Console.WriteLine("Müşteri Soyadı : " + customer.LastName);
+            Console.WriteLine("Müşteri Tc No : " + customer.TcNo);
+            Console.WriteLine("----------------------------");
+        }
+
+        public void Remove(Customer customer)
+        {
+            Console.WriteLine("Müşteri Silindi..");
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Müşteri ID : " + customer.Id);
+            Console.WriteLine("Müşteri Adı : " + customer.FirstName);
+            Console.WriteLine("Müşteri Soyadı : " + customer.LastName);
+            Console.WriteLine("Müşteri Tc No : " + customer.TcNo);
+            Console.WriteLine("----------------------------");
+        }
+
+        public void CustomerList(Customer customer)
+        {
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("Müşteri ID : " + customer.Id);
+            Console.WriteLine("Müşteri Adı : " + customer.FirstName);
+            Console.WriteLine("Müşteri Soyadı : " + customer.LastName);
+            Console.WriteLine("Müşteri Tc No : " + customer.TcNo);
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.FirstName = "İlkay";
+            customer1.LastName = "gl";
+            customer1.TcNo = "123456789";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.FirstName = "Gizem";
+            customer2.LastName = "Dek";
+            customer2.TcNo = "13567984";
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.FirstName = "Ahmet";
+            customer3.LastName = "gl";
+            customer3.TcNo = "123456789";
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+            customerManager.Remove(customer3);
+
+            Console.WriteLine("Müşteriler Listelendi!");
+
+            Customer[] customers = { customer1, customer2, customer3 };
+            foreach (var c in customers)
+            {
+                customerManager.CustomerList(c);
+            }
+        }
+    }
+}
