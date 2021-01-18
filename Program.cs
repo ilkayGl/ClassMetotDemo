021d2ca3c620874c738f0adb015e58f5473b1e00
ilkay gül <ilkaygl@outlook.com>
2021-01-18 16:17:34 +0300

    16 Ocak Ders sonu

--- Program.cs
+++ Program.cs
@@ -6,23 +6,8 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Customer customer1 = new Customer();
-            customer1.Id = 1;
-            customer1.FirstName = "İlkay";
-            customer1.LastName = "gl";
-            customer1.TcNo = "123456789";
-
-            Customer customer2 = new Customer();
-            customer2.Id = 2;
-            customer2.FirstName = "Gizem";
-            customer2.LastName = "Dek";
-            customer2.TcNo = "13567984";
-
-            Customer customer3 = new Customer();
-            customer3.Id = 3;
-            customer3.FirstName = "Ahmet";
-            customer3.LastName = "gl";
-            customer3.TcNo = "123456789";
+            Customer customer1, customer2, customer3;
+            Musteriler(out customer1, out customer2, out customer3);
 
             CustomerManager customerManager = new CustomerManager();
             customerManager.Add(customer1);
@@ -36,6 +21,27 @@ namespace ClassMetotDemo
             {
                 customerManager.CustomerList(c);
             }
+        }
+
+        private static void Musteriler(out Customer customer1, out Customer customer2, out Customer customer3)
+        {
+            customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.FirstName = "İlkay";
+            customer1.LastName = "gl";
+            customer1.TcNo = "123456789";
+
+            customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.FirstName = "Gizem";
+            customer2.LastName = "Dek";
+            customer2.TcNo = "13567984";
+
+            customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.FirstName = "Ahmet";
+            customer3.LastName = "gl";
+            customer3.TcNo = "123456789";
         }
     }
 }
